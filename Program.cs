bc952c8ec8ec9125fe921b757cc9f7db261ea979
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-08-14 18:36:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new Dictionary<string, int>();
+
+            while (true) 
+            {
+                Console.WriteLine("Material and amount:");
+                string material = Console.ReadLine();
+                if (material == "stop")
+                {
+                    break;
+                }
+                int value = int.Parse(Console.ReadLine());
+                if (array.ContainsKey(material))
+                {
+                    array[material] +=value;
+                }
+                else
+                {
+                    array.Add(material, value);
+                }          
+            }
+
+            foreach(var item in array)
+            {
+                Console.WriteLine($"{item.Key} --> {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CountCharsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word/s:");
+            string[] words = Console.ReadLine().Split(" ").ToArray();
+            List<char> characters = new List<char>();
+            List<char> temp = new List<char>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for(int j = 0; j < words[i].Length; j++)
+                {
+                    temp = words[i].ToList();
+                    characters.Add(temp[j]);
+                }
+                
+            }
+
+            var count = new Dictionary<char, int>();
+
+            foreach(char c in characters)
+            {
+                if (count.ContainsKey(c))
+                {
+                    count[c]++;
+                }
+                else
+                {
+                    count[c] = 1;
+                }
+            }
+            foreach(var v in count)
+            {
+                Console.WriteLine($"{v.Key} ->{v.Value}");
+            }
+        }
+    }
+}
