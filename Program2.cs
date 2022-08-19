130637f1b8b73ecb7919986bb128d55b258c77a2
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-08-20 00:04:49 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Order:");
+
+            string[] order;
+            string name;
+            double price;
+            double quantity;
+
+            var finalOrder = new Dictionary<string, double>();
+            var tempPrice = new Dictionary<string, double>();
+            var tempQuantity = new Dictionary<string, double>();
+
+            while (true)
+            {
+                order = Console.ReadLine().Split(" ").ToArray();
+
+                if (order[0] == "buy")
+                {
+                    break;
+                }
+
+
+                name = order[0];
+                price = double.Parse(order[1]);
+                quantity = double.Parse(order[2]);
+                double finalPrice;             
+
+                
+                if (finalOrder.ContainsKey(name))
+                {
+                    
+                    if(price != tempPrice[name])
+                    {                        
+                        quantity = quantity+ tempQuantity[name];
+                        finalPrice = price * quantity;
+                        finalOrder[name] = finalPrice;
+                    }
+                    if(price == tempPrice[name])
+                    {
+                        quantity = quantity + tempQuantity[name];
+                        finalPrice = price * quantity;
+                        finalOrder[name] = finalPrice;
+                    }
+                }
+
+                else
+                {
+                    tempPrice.Add(name, price);
+                    tempQuantity.Add(name, quantity);
+
+                    finalPrice = price * quantity;
+                    finalOrder.Add(name, finalPrice);
+                }                
+            }
+
+            foreach(var o in finalOrder)
+            {
+                Console.WriteLine($"{o.Key} --> {String.Format("{0:0.00}", o.Value)}");
+            }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of commands:");
+            int n = int.Parse(Console.ReadLine());
+
+            string name;
+            double grade;
+            
+
+
+            var finalGrade = new Dictionary<string, double>();
+            var temp = new Dictionary<string, double>();
+            var averageGrade = new Dictionary<string, double>();
+            
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Name:");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Grade:");
+                grade = double.Parse(Console.ReadLine());
+
+                if (finalGrade.ContainsKey(name))
+                {
+                    finalGrade[name]= finalGrade[name] + grade;
+                    temp[name]++;
+                }
+
+                else
+                {
+                    finalGrade.Add(name, grade);
+                    temp.Add(name, 1);
+                    averageGrade.Add(name, grade);
+                }
+
+
+            }
+
+            foreach(var m in temp)
+            {
+                finalGrade[m.Key] = finalGrade[m.Key] / m.Value;
+            }
+
+            foreach(var f in finalGrade)
+            {
+                if (finalGrade[f.Key] >= 4.50)
+                {
+                    Console.WriteLine($"{f.Key} -> {String.Format("{0:0.00}", f.Value)}");
+                }
+                
+            }
+                    
+        }
+    }
+}
